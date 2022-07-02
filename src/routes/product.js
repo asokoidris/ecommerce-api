3ff5903a7b9bba03c36e3c759889517b3c9968c0
unknown <asokoidris98@gmail.com>
2022-07-02 11:46:33 +0100

    change in all mode

---
+++ src/routes/product.js
@@ -0,0 +1,21 @@
+const joi = require ('@hapi/joi');
+const { object } = require('joi');
+
+
+
+const productValidation = data => {
+    const schema = joi.object({
+    title: joi.string()
+         .required() 
+         .unique(),
+     desc: joi.string()
+         .required(),
+     img: joi.string().required(),
+     categories: joi.array(),
+      size: joi.string(),
+     color: joi.string(),
+     price: joi.number()
+    })
+}
+
+module.exports =   productValidation;
