57e6935a840373947043d67507c8ce0aea37b347
asokoidris <asokoidris98@gmail.com>
2022-07-26 00:19:08 +0100

    chores:changes authController

--- src/controller/userAuth.js
+++ src/controller/userAuth.js
@@ -1,4 +1,4 @@
-const jwt = require ('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs');
 const User = require('../model/users')
 
@@ -6,54 +6,54 @@ const User = require('../model/users')
  * @description Authentication Controller
  */
 
- class UserAuthController {
+class UserAuthController {
     /**
      * @description return a JSON data
      * @param {Object} req - HTTP Request
      * @param {Object} res - HTTP Response
      * @return {Object} Returned object
      */
-static async userSignUp(req, res) {
-    try{
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    static async userSignUp(req, res) {
+        try {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-      const newUser = await new User({
-          username: req.body.username,
-          email: req.body.email,
-          phoneNumber: req.body.phoneNumber,
-          password: hashedPassword,
-      });
-      const savedUser = await newUser.save();
-      res.status(200).json(savedUser)
+            const newUser = await new User({
+                username: req.body.username,
+                email: req.body.email,
+                phoneNumber: req.body.phoneNumber,
+                password: hashedPassword,
+            });
+            const savedUser = await newUser.save();
+            res.status(200).json(savedUser)
+        }
+        catch (error) {
+            res.status(500).json(error.message)
+        }
+
     }
- catch (error) {
-  res.status(500).json(error.message)
-}
 
- }
- 
 
-static async userSignIn(req, res)  {
-    try {
-        const user = await User.findOne({email:req.body.email});
-        !user && res.status(404).json('user not found');
+    static async userSignIn(req, res) {
+        try {
+            const user = await User.findOne({ email: req.body.email });
+            !user && res.status(404).json('user not found');
 
-        const validPassword = await bcrypt.compare(req.body.password, user.password);
-        !validPassword && res.status(404).json('Wrong password');
-        const accessToken = jwt.sign({
-            id: user._id
-        },
-            process.env.JWT_SEC,
-           process.env.EXP_SEC
-        );
+            const isMatchPassword = await bcrypt.compare(req.body.password, user.password);
+            !isMatchPassword && res.status(404).json('Wrong password');
+            const accessToken = jwt.sign({
+                id: user._id
+            },
+                process.env.JWT_SEC,
+                process.env.EXP_SEC
+            );
 
-        const {password,updateAt, ...others} = user._doc;
+            const { password, updateAt, ...others } = user._doc;
 
-        res.status(200).json({...others, accessToken})
-    } catch (error) {
-        res.status(500).json(error.message)
+            res.status(200).json({ ...others, accessToken })
+        } catch (error) {
+            res.status(500).json(error.message)
+        }
     }
-}
 }
 module.exports = UserAuthController;
